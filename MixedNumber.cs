88c06a62d2906b2350ac7533d4f3145b545882cb
User <User@WIN-4QK4E4D33BC>
2024-02-03 19:58:08 +0400

    Добавьте файлы проекта.

---
+++ MixedNumber.cs
@@ -0,0 +1,90 @@
+﻿namespace FractionCalc
+{
+    class MixedNumber
+    {
+        private int wholePart;
+        private int numerator;
+        private int denominator;
+
+        public MixedNumber(int wholePart, int numerator, int denominator)
+        {
+            this.wholePart = wholePart;
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public static MixedNumber operator +(MixedNumber a, MixedNumber b)
+        {
+            int newWholePart = a.wholePart + b.wholePart;
+            int newNumerator = a.numerator * b.denominator + b.numerator * a.denominator;
+            int newDenominator = a.denominator * b.denominator;
+
+            
+            SimplifyFraction(ref newNumerator, ref newDenominator);
+
+            return new MixedNumber(newWholePart, newNumerator, newDenominator);
+        }
+
+        public static MixedNumber operator -(MixedNumber a, MixedNumber b)
+        {
+            int newWholePart = a.wholePart - b.wholePart;
+            int newNumerator = a.numerator * b.denominator - b.numerator * a.denominator;
+            int newDenominator = a.denominator * b.denominator;
+
+            
+            SimplifyFraction(ref newNumerator, ref newDenominator);
+
+            return new MixedNumber(newWholePart, newNumerator, newDenominator);
+        }
+
+        public static MixedNumber operator *(MixedNumber a, MixedNumber b)
+        {
+            int newWholePart = a.wholePart * b.wholePart;
+            int newNumerator = (a.wholePart * a.denominator + a.numerator) * (b.wholePart * b.denominator + b.numerator);
+            int newDenominator = a.denominator * b.denominator;
+
+            
+            SimplifyFraction(ref newNumerator, ref newDenominator);
+
+            return new MixedNumber(newWholePart, newNumerator, newDenominator);
+        }
+
+        public static MixedNumber operator /(MixedNumber a, MixedNumber b)
+        {
+            int newWholePart = a.wholePart / b.wholePart;
+            int newNumerator = (a.wholePart * a.denominator + a.numerator) * b.denominator;
+            int newDenominator = (b.wholePart * b.denominator + b.numerator) * a.denominator;
+
+            
+            SimplifyFraction(ref newNumerator, ref newDenominator);
+
+            return new MixedNumber(newWholePart, newNumerator, newDenominator);
+        }
+
+        
+        private static void SimplifyFraction(ref int numerator, ref int denominator)
+        {
+            int gcd = GCD(numerator, denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+        }
+
+        
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return $"{wholePart} {numerator}/{denominator}";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FractionCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MixedNumber a = new MixedNumber(2, 1, 2); // 2 1/2
+            MixedNumber b = new MixedNumber(1, 3, 4); // 1 3/4
+
+            MixedNumber sum = a + b;
+            MixedNumber difference = a - b;
+            MixedNumber product = a * b;
+            MixedNumber quotient = a / b;
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Difference: {difference}");
+            Console.WriteLine($"Product: {product}");
+            Console.WriteLine($"Quotient: {quotient}");
+        }
+    }
+}
